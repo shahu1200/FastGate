fae5db29cf8450bdd7f0eb6b9baefa783057d1a1
shahuraje yadav <107189580+shahu1200@users.noreply.github.com>
2022-06-09 18:55:11 +0530

    Create version.py

---
+++ app/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Application Version
+
+    NOTE: THIS FILE IS CREATED THROUGH BASH
+"""
+
+APP_VERSION = "1.0.0"
+APP_BUILD = "02/06/2022 19:00:28"
